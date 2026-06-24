880f122c3e876759ab09e09ba3cc29ab318a1f02
n4v4ss <jnavarretebalcazar@gmail.com>
2026-06-23 23:27:42 -0500

    problema4

--- Solucion_Codigo/Proyect_Taller07/src/Problema_4_EjecutorTrabajador.java
+++ Solucion_Codigo/Proyect_Taller07/src/Problema_4_EjecutorTrabajador.java
@@ -5,24 +5,23 @@
  * @author Jhandry Navarrete
  * @version 1.0
  */
-
 public class Problema_4_EjecutorTrabajador {
     public static void main(String[] args) {
-        // Instancias de prueba
+        // Instancia del jefe
         Jefe jefe = new Jefe("Ana", "Perez", "Calle Falsa 123", "12345678A", 3000.0);
         
+        // Instancias de trabajadores (ahora todos heredan de Trabajador)
         FijoMensual t1 = new FijoMensual("Luis", "Garcia", "Av. Siempre Viva 1", "87654321B", 1200.0);
         t1.setJefe(jefe);
         
         PorHoras t2 = new PorHoras("Maria", "Lopez", "Plaza Central 5", "11223344C", 10.0, 15.0);
-        t2.setHoras(45.0); // 40 normales + 5 extras
+        t2.setHoras(45.0); 
         t2.setJefe(jefe);
 
         Comisionista t3 = new Comisionista("Carlos", "Ruiz", "Calle Real 10", "55667788D", 0.10);
         t3.setVentas(5000.0);
         t3.setJefe(jefe);
 
-        // Imprimir nóminas
         System.out.println("Nómina de " + t1.getNombre() + ": $" + t1.calcularSueldo());
         System.out.println("Nómina de " + t2.getNombre() + ": $" + t2.calcularSueldo());
         System.out.println("Nómina de " + t3.getNombre() + ": $" + t3.calcularSueldo());
@@ -31,17 +30,21 @@ public class Problema_4_EjecutorTrabajador {
 
 abstract class Trabajador {
     protected String nombre, apellidos, direccion, dni;
-    protected Jefe jefe;
+    // CORRECCIÓN: El jefe es un Trabajador, no específicamente un Jefe (evita dependencias circulares)
+    protected Trabajador jefe; 
 
     public Trabajador(String nombre, String apellidos, String direccion, String dni) {
         this.nombre = nombre; this.apellidos = apellidos;
         this.direccion = direccion; this.dni = dni;
     }
-    public void setJefe(Jefe j) { this.jefe = j; }
+    
+    // CORRECCIÓN: Acepta cualquier Trabajador como jefe
+    public void setJefe(Trabajador j) { this.jefe = j; }
     public String getNombre() { return nombre; }
     public abstract double calcularSueldo();
 }
 
+// Ahora Jefe también es un Trabajador
 class Jefe extends Trabajador {
     private double sueldoFijo;
     public Jefe(String n, String a, String d, String dn, double s) { super(n, a, d, dn); this.sueldoFijo = s; }
@@ -75,7 +78,8 @@ class PorHoras extends Trabajador {
 
 /**
  * run:
- * Nómina de Luis: $1200.0
- * Nómina de Maria: $475.0
- * Nómina de Carlos: $500.0
+N�mina de Luis: $1200.0
+N�mina de Maria: $475.0
+N�mina de Carlos: $500.0
+BUILD SUCCESSFUL (total time: 0 seconds)
  */
